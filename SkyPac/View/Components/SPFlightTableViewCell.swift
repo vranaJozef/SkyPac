48a902eaaaafacc3050050a2bb23347eb756dae4
Jozef Vrana <vranajozef7652@gmail.com>
2019-07-26 11:36:01 +0200

    Project organization

---
+++ SkyPac/View/Components/SPFlightTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SPFlightTableViewCell.swift
+//  SkyPac
+//
+//  Created by Jozef Vrana on 26/07/2019.
+//  Copyright © 2019 Jozef Vrana. All rights reserved.
+//
+
+import UIKit
+
+class SPFlightTableViewCell: UITableViewCell {
+    
+}
---
+++ SkyPac/View/ViewControllers/SPFlight.swift
@@ -0,0 +1,14 @@
+//
+//  SPFlight.swift
+//  SkyPac
+//
+//  Created by Jozef Vrana on 26/07/2019.
+//  Copyright © 2019 Jozef Vrana. All rights reserved.
+//
+
+import Foundation
+
+struct SPFlight: Decodable {
+    
+}
+
